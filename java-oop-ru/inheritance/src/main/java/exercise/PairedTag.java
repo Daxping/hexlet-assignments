e83e3b2d20bfb011bb191c01ef0acc7a7146fafa
@hexlet/cli <support@hexlet.io>
2022-10-31 12:25:03 +0300

    submit java-oop-ru/inheritance

---
+++ java-oop-ru/inheritance/src/main/java/exercise/PairedTag.java
@@ -0,0 +1,26 @@
+package exercise;
+
+import java.util.Map;
+import java.util.List;
+
+// BEGIN
+public class PairedTag extends Tag{
+
+    PairedTag(String name, Map<String, String> attributes, String body, List<Tag> tags) {
+        this.name = name;
+        this.attributes = attributes;
+        this.body = body;
+        this.tags = tags;
+    }
+
+    @Override
+    public String toString() {
+        String str = "";
+        for (Tag tag : tags) {
+            str = str + tag.toString();
+        }
+
+        return super.toString() + body + str + "</" + name + ">";
+    }
+}
+// END
---
+++ java-oop-ru/inheritance/src/main/java/exercise/SingleTag.java
@@ -0,0 +1,20 @@
+package exercise;
+
+import java.util.Map;
+import java.lang.String;
+import java.lang.StringBuilder;
+
+
+// BEGIN
+public class SingleTag extends Tag{
+    SingleTag(String name, Map<String, String> attributes) {
+        this.name = name;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
+// END
